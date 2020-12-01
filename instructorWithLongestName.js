2ec4319c9b33ef5a815e535d5b94451eaad90be4
HaopengSun <sunhaopeng2016@gmail.com>
2020-12-01 14:41:42 +0000

    instructorWithLongestName

---
+++ instructorWithLongestName.js
@@ -0,0 +1,24 @@
+const instructorWithLongestName = function(instructors) {
+    let longest = 0;
+    let instructor;
+    for (let i = 0; i < instructors.length; i++){
+        if (instructors[i].name.length > longest){
+            longest = instructors[i].name.length;
+            instructor = instructors[i]
+        }
+    }
+    return instructor;
+
+};
+  
+  console.log(instructorWithLongestName([
+    {name: "Samuel", course: "iOS"},
+    {name: "Jeremiah", course: "Web"},
+    {name: "Ophilia", course: "Web"},
+    {name: "Donald", course: "Web"}
+  ]));
+  console.log(instructorWithLongestName([
+    {name: "Matthew", course: "Web"},
+    {name: "David", course: "iOS"},
+    {name: "Domascus", course: "Web"}
+  ]));
